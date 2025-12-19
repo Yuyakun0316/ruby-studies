a6b342cd15a773a306af27652f777d0ead812408
Yuyakun0316 <3.16.yuyakun@gmail.com>
2025-12-19 16:27:28 +0900

    変数の式展開と結合の実験コードを追加

---
+++ variable_sample.rb
@@ -0,0 +1,12 @@
+# 変数の実験
+# 変数とは・・・データを一時的に保存しておく箱のような物
+# 文字列の結合について確認する
+
+first_name = "Taro"
+last_name = "Yamada"
+
+# 式展開（#{}）を使うパターン
+puts "私の名前は #{last_name} #{first_name} です"
+
+# プラス（+）でつなぐパターン
+puts "私の名前は " + last_name + " " + first_name + " です"
